fe4cd0edfbcb691a4c8292f9a035c1ced3a9f9b3
tangjiapeng <tjpcorner@126.com>
2016-12-06 17:56:46 +0800

    初始化项目

---
+++ TJPYingKe/TJPYingKe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TJPYingKe
+//
+//  Created by Walkman on 2016/12/6.
+//  Copyright © 2016年 AaronTang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
